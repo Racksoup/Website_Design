bed36879d82eb69b722ad15235802a27b59634a9
Racksoup <rack.connor1@gmail.com>
2022-12-07 18:29:02 -0500

    sytle header

--- client/src/UI/Components/Header/Header.jsx
+++ client/src/UI/Components/Header/Header.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import './header.scss';
+import HeaderBG from '../../../images/HeaderBG.png';
 
 const Header = ({ Ref, contactRef, linkToRef }) => {
   return (
     <div>
-      <div className='Header-Spacing' />
       <div className='Header' ref={Ref}>
+        <img src={HeaderBG} alt='background' className='background' />
         <div className='Content'>
-          <div className='Text'>HI,</div>
-          <div className='Text'>I'M CONNOR RACK</div>
-          <div className='Text'>WEB DEVELOPER</div>
-          <div className='Btn' onClick={() => linkToRef(contactRef)}>
-            CONTACT ME!
-          </div>
+          <div className='Text Text-Small'>My name is</div>
+          <div className='Text Text-Big'>CONNOR RACK</div>
+          <div className='Text Text-Small'>and I am a</div>
+          <div className='Text Text-Big'>WEB DEVELOPER</div>
+        </div>
+        <div className='Btn' onClick={() => linkToRef(contactRef)}>
+          CONTACT ME!
         </div>
       </div>
     </div>
--- client/src/UI/Components/Navbar/Navbar.jsx
+++ client/src/UI/Components/Navbar/Navbar.jsx
@@ -1,49 +1,32 @@
 import React from 'react';
-import './navbar.scss';
+import './Navbar.scss';
+import Panda from '../../../images/Panda.png';
 
-const NavBar = ({
-  headerRef,
-  servicesRef,
-  projectsRef,
-  testimonialsRef,
-  aboutRef,
-  blogsRef,
-  contactRef,
-  linkToRef,
-}) => {
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
   return (
-    <div className='NavBar'>
+    <div className='Navbar'>
       <div className='Content'>
-        <div />
-        <div className='Name'>CONNOR RACK</div>
-        <div />
-        <div className='Links'>
-          <div className='RefLink' onClick={() => linkToRef(headerRef)}>
-            Home
-          </div>
-          <div className='RefLink' onClick={() => linkToRef(servicesRef)}>
-            Services
-          </div>
-          <div className='RefLink' onClick={() => linkToRef(projectsRef)}>
-            Portfolio
-          </div>
-          <div className='RefLink' onClick={() => linkToRef(testimonialsRef)}>
-            Testimonials
-          </div>
-          <div className='RefLink' onClick={() => linkToRef(aboutRef)}>
-            About
-          </div>
-          <div className='RefLink' onClick={() => linkToRef(blogsRef)}>
-            Blogs
-          </div>
-          <div className='RefLink' onClick={() => linkToRef(contactRef)}>
-            Contact
-          </div>
+        <div className='Left'>
+          <button className='Title'>
+            <img src={Panda} alt='Panda' />
+            <h4>Panda NFT's</h4>
+          </button>
+          <Link className='Link' to='/Categories'>
+            <p className='Item'>Categories</p>
+          </Link>
+          <p className='Item'>Merch</p>
+          <p className='Item Item-End'>Popular</p>
         </div>
-        <div />
+
+        <div className='Right'>
+          <button className='Sign-up'>Sign-up</button>
+          <button className='Sign-in'>Sign-in</button>
+        </div>
       </div>
     </div>
   );
 };
 
-export default NavBar;
+export default Navbar;
