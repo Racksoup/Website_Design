a4e6c17dcb375cef20b58addcd19f1239706eeb1
Racksoup <rack.connor1@gmail.com>
2022-12-08 17:04:28 -0500

    new_website

---
+++ src/UI/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+      <div className='Content'></div>
+      <div className='EndBG' />
+    </div>
+  );
+};
+
+export default Footer;
--- src/UI/Pages/Home.jsx
+++ src/UI/Pages/Home.jsx
@@ -1,67 +1,8 @@
-import React, { useState, useRef, useEffect } from 'react';
-//import NavBar from '../Components/NavBar/NavBar.jsx';
-import Header from '../Components/Header/Header.jsx';
-import Services from '../Components/Services/Services.jsx';
-import Projects from '../Components/Projects/Projects.jsx';
-//import Testimonials from '../Components/Testimonials/Testimonials.jsx';
-import About from '../Components/About/About.jsx';
-import Blogs from '../Components/Blogs/Blogs.jsx';
-import Contact from '../Components/Contact/Contact.jsx';
-import ContactBtn from '../Components/ContactBtn/ContactBtn.jsx';
+import React from 'react';
+import './Home.scss';
 
 const Home = () => {
-  const [scrollPos, setScrollPos] = useState(0);
-  const handleScroll = () => {
-    const pos = window.pageYOffset;
-    setScrollPos(pos);
-  };
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true });
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  const headerRef = useRef(null);
-  const servicesRef = useRef(null);
-  const projectsRef = useRef(null);
-  const testimonialsRef = useRef(null);
-  const aboutRef = useRef(null);
-  const blogsRef = useRef(null);
-  const contactRef = useRef(null);
-
-  const linkToRef = (ref) => {
-    const linkOffset = 100;
-    const refPosition = ref.current.getBoundingClientRect().top;
-    const offsetPosition = refPosition + window.pageYOffset - linkOffset;
-    window.scrollTo({ top: offsetPosition, behavior: 'smooth' });
-  };
-
-  return (
-    <div>
-      {/* <NavBar
-        headerRef={headerRef}
-        servicesRef={servicesRef}
-        projectsRef={projectsRef}
-        testimonialsRef={testimonialsRef}
-        aboutRef={aboutRef}
-        blogsRef={blogsRef}
-        contactRef={contactRef}
-        linkToRef={linkToRef}
-      /> */}
-      {scrollPos > 500 && scrollPos < 4000 && (
-        <ContactBtn contactRef={contactRef} linkToRef={linkToRef} />
-      )}
-      <Header Ref={headerRef} contactRef={contactRef} linkToRef={linkToRef} />
-      <Services Ref={servicesRef} />
-      <Projects Ref={projectsRef} />
-      {/* <Testimonials Ref={testimonialsRef} /> */}
-      <About Ref={aboutRef} />
-      <Blogs Ref={blogsRef} />
-      <Contact Ref={contactRef} />
-    </div>
-  );
+  return <div className='Home'>Home</div>;
 };
 
 export default Home;
