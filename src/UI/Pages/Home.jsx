7cb80722e8361470d69176180cc961e0db1d33f1
Racksoup <rack.connor1@gmail.com>
2022-12-08 17:25:10 -0500

    starting section0

--- src/UI/Pages/Home.jsx
+++ src/UI/Pages/Home.jsx
@@ -2,7 +2,25 @@ import React from 'react';
 import './Home.scss';
 
 const Home = () => {
-  return <div className='Home'>Home</div>;
+  return (
+    <div className='Home'>
+      <div className='HomeBox'>
+        <div className='Header'>
+          <div className='NameBox'>
+            <h2>CONNOR RACK</h2>
+          </div>
+
+          <div className='HeaderNav'>
+            <p>home</p>
+            <p>services</p>
+            <p>portfolio</p>
+            <p>about</p>
+            <p>contact</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
