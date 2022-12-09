172da3ba6555ecaac4c07fc2c3e93726bb598858
Racksoup <rack.connor1@gmail.com>
2022-12-09 10:50:29 -0500

    Section5 Func

--- src/UI/Pages/Home/Section5.jsx
+++ src/UI/Pages/Home/Section5.jsx
@@ -11,42 +11,47 @@ const Section5 = () => {
         <h2>Blogs</h2>
       </div>
       <div className='Content'>
-        <div className='Card-2'>
-          <div className='CardHeader'>
-            <h4>Testimonial</h4>
-          </div>
-          <img src={DevBlog} alt='DevBlog' />
-
-          <div className='CardContent'>
-            <div className='TimeRow'>
-              <Icon icon='ic:baseline-access-time' className='Icon' />
-              <p className='Text'>05/30/23</p>
+        <a href='https://dev-blog-jib0.onrender.com/' target='_blank'>
+          <div className='Card-2'>
+            <div className='CardHeader'>
+              <h4>Testimonial</h4>
             </div>
-            <h4>LOREM IPSUM</h4>
-            <p className='Text Desc'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore.
-            </p>
-          </div>
-        </div>
-        <div className='Card-2'>
-          <div className='CardHeader'>
-            <h4>Testimonial</h4>
-          </div>
-          <img src={DevBlog} alt='DevBlog' />
+            <img src={DevBlog} alt='DevBlog' />
 
-          <div className='CardContent'>
-            <div className='TimeRow'>
-              <Icon icon='ic:baseline-access-time' className='Icon' />
-              <p className='Text'>05/30/23</p>
+            <div className='CardContent'>
+              <div className='TimeRow'>
+                <Icon icon='ic:baseline-access-time' className='Icon' />
+                <p className='Text'>05/30/23</p>
+              </div>
+              <h4>LOREM IPSUM</h4>
+              <p className='Text Desc'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore.
+              </p>
             </div>
-            <h4>LOREM IPSUM</h4>
-            <p className='Text Desc'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore.
-            </p>
           </div>
-        </div>
+        </a>
+
+        <a href='https://history-blog.onrender.com/' target='_blank'>
+          <div className='Card-2'>
+            <div className='CardHeader'>
+              <h4>Testimonial</h4>
+            </div>
+            <img src={DevBlog} alt='DevBlog' />
+
+            <div className='CardContent'>
+              <div className='TimeRow'>
+                <Icon icon='ic:baseline-access-time' className='Icon' />
+                <p className='Text'>05/30/23</p>
+              </div>
+              <h4>LOREM IPSUM</h4>
+              <p className='Text Desc'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore.
+              </p>
+            </div>
+          </div>
+        </a>
       </div>
     </div>
   );
