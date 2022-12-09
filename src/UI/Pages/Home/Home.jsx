b80efd3e6ffedc1ab8ab1fe52abecc834d1ebeaf
Racksoup <rack.connor1@gmail.com>
2022-12-09 01:01:41 -0500

    Section2

--- src/UI/Pages/Home/Home.jsx
+++ src/UI/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.scss';
 import Section0 from './Section0';
 import Section1 from './Section1';
 import Section2 from './Section2';
+import Section3 from './Section3';
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Section0 />
       <Section1 />
       <Section2 />
+      <Section3 />
     </div>
   );
 };
--- src/UI/Pages/Home/Section2.jsx
+++ src/UI/Pages/Home/Section2.jsx
@@ -1,8 +1,63 @@
 import React from 'react';
 import './Section2.scss';
 
+import WebsiteItem from '../../../images/WebsiteItem.png';
+
 const Section2 = () => {
-  return <div className='Section2'></div>;
+  return (
+    <div className='Section2'>
+      <div className='Content'>
+        <div className='HeaderBox'>
+          <h2>Portfolio</h2>
+        </div>
+
+        <div className='Buttons'>
+          <div className='TiltBtn'>
+            <p>All</p>
+          </div>
+          <div className='TiltBtn TiltBtn-1'>
+            <p>Resturant</p>
+          </div>
+          <div className='TiltBtn'>
+            <p>law</p>
+          </div>
+          <div className='TiltBtn TiltBtn-1'>
+            <p>ecommerce</p>
+          </div>
+          <div className='TiltBtn'>
+            <p>beauty</p>
+          </div>
+        </div>
+
+        <div className='Websites'>
+          <div className='ImageItem ImageItem-Space'>
+            <img src={WebsiteItem} alt='WebsiteItem' />
+            <div className='Title'>Panda NFT's</div>
+          </div>
+          <div className='ImageItem ImageItem-Space'>
+            <img src={WebsiteItem} alt='WebsiteItem' />
+            <div className='Title'>Panda NFT's</div>
+          </div>
+          <div className='ImageItem ImageItem-Space'>
+            <img src={WebsiteItem} alt='WebsiteItem' />
+            <div className='Title'>Panda NFT's</div>
+          </div>
+          <div className='ImageItem'>
+            <img src={WebsiteItem} alt='WebsiteItem' />
+            <div className='Title'>Panda NFT's</div>
+          </div>
+          <div className='ImageItem'>
+            <img src={WebsiteItem} alt='WebsiteItem' />
+            <div className='Title'>Panda NFT's</div>
+          </div>
+          <div className='ImageItem'>
+            <img src={WebsiteItem} alt='WebsiteItem' />
+            <div className='Title'>Panda NFT's</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Section2;
