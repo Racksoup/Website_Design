d9744deb8d7b2f94300ade745c5e02dd9df2ad6d
Racksoup <rack.connor1@gmail.com>
2022-12-09 01:59:03 -0500

    Section5

--- src/UI/Pages/Home/Section5.jsx
+++ src/UI/Pages/Home/Section5.jsx
@@ -1,8 +1,55 @@
 import React from 'react';
 import './Section5.scss';
+import DevBlog from '../../../images/DevBlog.png';
+
+import { Icon } from '@iconify/react';
 
 const Section5 = () => {
-  return <div className='Section5'></div>;
+  return (
+    <div className='Section5'>
+      <div className='HeaderBox'>
+        <h2>Blogs</h2>
+      </div>
+      <div className='Content'>
+        <div className='Card-2'>
+          <div className='CardHeader'>
+            <h4>Testimonial</h4>
+          </div>
+          <img src={DevBlog} alt='DevBlog' />
+
+          <div className='CardContent'>
+            <div className='TimeRow'>
+              <Icon icon='ic:baseline-access-time' className='Icon' />
+              <p className='Text'>05/30/23</p>
+            </div>
+            <h4>LOREM IPSUM</h4>
+            <p className='Text Desc'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore.
+            </p>
+          </div>
+        </div>
+        <div className='Card-2'>
+          <div className='CardHeader'>
+            <h4>Testimonial</h4>
+          </div>
+          <img src={DevBlog} alt='DevBlog' />
+
+          <div className='CardContent'>
+            <div className='TimeRow'>
+              <Icon icon='ic:baseline-access-time' className='Icon' />
+              <p className='Text'>05/30/23</p>
+            </div>
+            <h4>LOREM IPSUM</h4>
+            <p className='Text Desc'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Section5;
---
+++ src/UI/Pages/Home/Section6.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Section6.scss';
+
+const Section6 = () => {
+  return <div className='Section6'></div>;
+};
+
+export default Section6;
